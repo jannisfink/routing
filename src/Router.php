d76b8c8199e20f43c7620d0416531c9437c42fc2
Jannis Fink <fink@uni-bremen.de>
2016-03-08 10:33:19 +0100

    fix typo

--- src/Router.php
+++ src/Router.php
@@ -36,12 +36,12 @@ class Router {
   private static $classMap;
 
   /**
-   * Mail API method to show a page.
+   * Main API method to show a page.
    *
    * @param array $classMap an associative array which maps all wanted urls to the given classname
    */
   public static function showPage(array $classMap) {
-    static::$classMap = $classMap;
+    self::$classMap = $classMap;
   }
 
   /**
