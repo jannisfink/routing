d9f223ca79e2a807d49d85bdc57d768cc5e82e5c
Jannis Fink <fink@uni-bremen.de>
2016-12-11 20:27:51 +0100

    add header fields

--- src/http/Header.php
+++ src/http/Header.php
@@ -24,7 +24,43 @@ namespace Yarf\http;
  */
 final class Header {
 
+  const ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+  const ACCEPT_PATCH = "Accept-Patch";
+  const ACCEPT_RANGES = "Accept-Ranges";
+  const AGE = "Age";
+  const ALLOW = "Allow";
+  const ALT_SVC = "Alt-Svc";
+  const CACHE_CONTROL = "Cache-Control";
+  const CONNECTION = "Connection";
+  const CONTENT_DISPOSITION = "Content-Disposition";
+  const CONTENT_ENCODING = "Content-Encoding";
+  const CONTENT_LANGUAGE = "Content-Language";
+  const CONTENT_LENGTH = "Content-Length";
+  const CONTENT_LOCATION = "Content-Location";
+  const CONTENT_RANGE = "Content-Range";
   const CONTENT_TYPE = "Content-Type";
+  const DATE = "Date";
+  const ETAG = "ETag";
+  const EXPIRES = "Expires";
+  const LAST_MODIFIED = "Last-Modified";
+  const PRAGMA = "Pragma";
+  const PROXY_AUTHENTICATE = "Proxy-Authenticate";
+  const PUBLIC_KEY_PINS = "Public-Key-Pins";
+  const REFRESH = "Refresh";
+  const RETRY_AFTER = "Retry-After";
+  const SERVER = "Server";
+  const SET_COOKIE = "Set-Cookie";
+  const STATUS = "Status";
+  const STRICT_TRANSPORT_SECURITY = "Strict-Transport-Security";
+  const TRAILER = "Trailer";
+  const TRANSFER_ENCODING = "Transfer-Encoding";
+  const TSV = "TSV";
+  const UPGRADE = "Upgrade";
+  const VARY = "Vary";
+  const VIA = "Via";
+  const Warning = "Warning";
+  const WWW_AUTHENTICATE = "WWW-Authenticate";
+  const X_FRAME_OPTIONS = "X-Frame-Options";
 
   /**
    * Header constructor.
